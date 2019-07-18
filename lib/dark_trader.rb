cab1e5bed103acf182a8bdd836aa4016a8761feb
ekwayv8 <yvon.ekwalla@gmail.com>
2019-07-18 19:28:19 +0200

    MAJ du fichier Dark_trader

--- lib/dark_trader.rb
+++ lib/dark_trader.rb
@@ -6,43 +6,75 @@ require 'open-uri'
 
 Dotenv.load('.env')
 
-page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))   
-#puts page.class   # => Nokogiri::HTML::Document
+def get_data
+
+	page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))   
+	#puts page.class   # => Nokogiri::HTML::Document
+
+end
 
 
-#téléchargement de l'ensemble des symboles
-crypto_fullname_array = page.xpath('//*[@id]/td[3]')
+def extraction_symbol(page)
+		
+	#téléchargement de l'ensemble des symboles
+	crypto_fullname_array = page.xpath('//*[@id]/td[3]')
 
 
-# conservation du texte
-crypto_name_array = crypto_fullname_array.map { |string| string.text  }
-	#puts crypto_name_array[0]
+	# conservation du texte
+	crypto_name_array = crypto_fullname_array.map { |string| string.text  }
+		#puts crypto_name_array[0]
 
-#Téléchargement de l'ensemble des prix
-crypto_fullprice_array = page.xpath('//*[@id]/td[5]/a')
+	return crypto_name_array
 
+end
 
-# conservation des prix
-crypto_price_array = crypto_fullprice_array.map { |price| price.text}
-	#puts crypto_price_array
+def extraction_price(page)
+	
 
-
-# Pour convertir en float
-#crypto_price_array.collect do |value| 
- # value.to_f 
-#end
-#puts crypto_price_array[0].class
+	#Téléchargement de l'ensemble des prix
+	crypto_fullprice_array = page.xpath('//*[@id]/td[5]/a')
 
 
+	# conservation des prix
+	crypto_price_array = crypto_fullprice_array.map { |price| price.text  }
+		#puts crypto_price_array
 
-#Création de l'array final (contenant les hashes)
-crypto_array = []
+	return crypto_price_array
 
-crypto_name_array.each do |symbol|
+end
 
-result = { symbol => crypto_price_array[crypto_name_array.index(symbol)] }
-    crypto_array << result
 
-  end
 
-  puts crypto_array#[0]
+def crypto(crypto_name_array, crypto_price_array)
+
+	#Création de l'array final (contenant les hashes)
+	crypto_array = []
+
+	crypto_name_array.each do |symbol|
+
+	result = { symbol => crypto_price_array[crypto_name_array.index(symbol)] }
+	    crypto_array << result
+
+	 end
+	  puts crypto_array
+
+end
+
+
+
+def perform
+
+	page = get_data
+	crypto_name_array = extraction_symbol(page)
+	crypto_price_array = extraction_price(page)
+	extraction_price(page)
+	extraction_symbol(page)
+	crypto(crypto_name_array,crypto_price_array)
+	
+
+
+end
+
+perform
+
+
